cd0835b3a5a1e94a6f1351e9ea4f06761a98ad76
saif-dev-19 <anonymous.safu@gmail.com>
2026-08-24 11:38:41 +0600

    staticfiles added

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,3 +26,10 @@ urlpatterns = [
     path("api/", include("apps.dashboard.urls") ),
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT,
+    )
